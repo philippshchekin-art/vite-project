29ca9849e4cd4690c5f03c0d9aa66745334431f4
Philipp Shchekin <philipp.shchekin@gmail.com>
2025-12-30 16:44:44 -0500

    adding counter card

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Card from "./components/card.jsx";
+import Counter from "./components/counter.jsx";
 import { cards } from "./data/cardData.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <div className="card">
+      <Counter />
+    </div>
     <div className="cardContainer">
       {cards.map((card) => (
         <Card
